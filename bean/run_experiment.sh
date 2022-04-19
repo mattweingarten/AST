4628f02b7b4f3d93122250c60b1e356760cb92f3
Andrin Bertschi <abertschi@abertschi.ch>
2022-04-19 22:59:29 +0200

    experiment data for bean aka andrin

---
+++ bean/run_experiment.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+source $FUZZBENCH_HOME/.venv/bin/activate
+name="exp-$(date +%Y-%m-%d-%H-%M-%S)"
+echo $name
+
+PYTHONPATH=$FUZZBENCH_HOME python3 $FUZZBENCH_HOME/experiment/run_experiment.py \
+    --experiment-config $SCRIPT_DIR/experiment-config.bean.yaml  \
+    --experiment-name "$name"  \
+    --benchmarks freetype2-2017 \
+    --fuzzers aflo0
