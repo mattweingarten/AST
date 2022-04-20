cecdc0372d2be2310303ee24abba4448bb533bb0
Matthew Weingarten <wmatt@student.ethz.ch>
2022-04-20 09:58:26 +0200

    Change benchmark script

--- run_benchmark.py
+++ run_benchmark.py
@@ -1,3 +1,4 @@
+from pickle import GLOBAL
 import sys
 import re
 import os
@@ -6,21 +7,24 @@ import argparse
 from argparse import ArgumentParser
 
 
+
 COMMAND  = "PYTHONPATH=. python3 experiment/run_experiment.py --experiment-config {config}  --experiment-name {name} --benchmarks {benchmark}  --fuzzers {fuzzers}"  
 
 parser = ArgumentParser()
 parser.add_argument('--benchmarks', nargs='+', default=[])
 parser.add_argument('--fuzzers', nargs='+', default=[])
 parser.add_argument('--config', default="")
+parser.add_argument('--n', default=1)
 args = vars( parser.parse_args())
 
-fuzzers = " ".join(args['fuzzers'])
+# fuzzers = " ".join(args['fuzzers'])
 
 for benchmark in args['benchmarks']:
-    name = benchmark + "-"  +  fuzzers.replace(" ", "-")
-    name =  re.sub("[^0-9a-z-]+", "", name)
-    name = name[:29]
-    os.system(COMMAND.format(name=name,benchmark=benchmark,fuzzers=fuzzers,config=args['config']))
+    for fuzzer in args['fuzzers']:
+        name = benchmark + "-" +  fuzzer
+        name =  re.sub("[^0-9a-z-]+", "", name)
+        name = name[:28] + str(args['n'])
+        os.system(COMMAND.format(name=name,benchmark=benchmark,fuzzers=fuzzer,config=args['config']))
     # print(COMMAND.format(name=name,benchmark=benchmark,fuzzers=fuzzers,config=args['config']))
 
 
