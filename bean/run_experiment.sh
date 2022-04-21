151d73f73e74289346ac03f4e54a27ca0a46f7fe
Andrin Bertschi <abertschi@abertschi.ch>
2022-04-21 19:51:00 +0200

    notes

--- bean/run_experiment.sh
+++ bean/run_experiment.sh
@@ -12,4 +12,4 @@ PYTHONPATH=$FUZZBENCH_HOME python3 $FUZZBENCH_HOME/experiment/run_experiment.py 
     --experiment-config $SCRIPT_DIR/experiment-config.bean.yaml  \
     --experiment-name "$name"  \
     --benchmarks freetype2-2017 \
-    --fuzzers aflo0 aflo1 aflo2 aflo3
+    --fuzzers aflplusplus_ast_o0 aflplusplus_ast_o1 aflplusplus_ast_o2 aflplusplus_ast_o3
