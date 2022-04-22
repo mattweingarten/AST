cd076009a20cdb2ab11bca257d6c35f2bf93d082
Andrin Bertschi <abertschi@abertschi.ch>
2022-04-22 09:17:27 +0200

    benchmark data

--- bean/run_experiment.sh
+++ bean/run_experiment.sh
@@ -7,9 +7,10 @@ source $FUZZBENCH_HOME/.venv/bin/activate
 name="exp-$(date +%Y-%m-%d-%H-%M-%S)"
 echo $name
 
+ #  freetype2-2017 aspell_aspell_fuzzer \
 PYTHONPATH=$FUZZBENCH_HOME python3 $FUZZBENCH_HOME/experiment/run_experiment.py \
     --allow-uncommitted-changes \
     --experiment-config $SCRIPT_DIR/experiment-config.bean.yaml  \
     --experiment-name "$name"  \
-    --benchmarks freetype2-2017 \
+    --benchmarks libpng-1.2.56 \
     --fuzzers aflplusplus_ast_o0 aflplusplus_ast_o1 aflplusplus_ast_o2 aflplusplus_ast_o3
