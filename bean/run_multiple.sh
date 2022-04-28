8ec9370e1e5e89c57a123946e5ec4d0930dd98d0
Andrin Bertschi <abertschi@abertschi.ch>
2022-04-28 10:06:29 +0200

    data for libhtp fuzz and lcms 4h

--- bean/run_multiple.sh
+++ bean/run_multiple.sh
@@ -4,7 +4,10 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 RUN=$SCRIPT_DIR/run_experiment.sh
 
-$RUN file_magic_fuzzer
+$RUN libhtp_fuzz_htp
+$RUN stb_stbi_read_fuzzer
+$RUN poppler_pdf_fuzzer
+
 # $RUN freetype2-2017
 # $RUN lcms-2017-03-21
 # $RUN libjpeg-turbo-07-2017
