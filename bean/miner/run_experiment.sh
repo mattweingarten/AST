6e540a59ac7e1167eb3776a98ca2d1914384980c
Andrin Bertschi <abertschi@abertschi.ch>
2022-05-27 17:23:50 +0200

    miner measure init

--- bean/miner/run_experiment.sh
+++ bean/miner/run_experiment.sh
@@ -7,14 +7,14 @@ source $FUZZBENCH_HOME/.venv/bin/activate
 name="exp-$(date +%Y-%m-%d-%H-%M-%S)"
 echo $name
 
-# fuzzers="aflplusplus_ast_o3 aflplusplus_ast_o2 aflplusplus_ast_o1 aflplusplus_ast_o0 aflplusplus_ast_f0 aflplusplus_ast_f1"
-fuzzers="aflplusplus_ast_f0 aflplusplus_ast_f1"
+fuzzers="aflplusplus_ast_o3 aflplusplus_ast_o2 aflplusplus_ast_o1 aflplusplus_ast_o0 aflplusplus_ast_f0 aflplusplus_ast_f1"
+# fuzzers="aflplusplus_ast_f0 aflplusplus_ast_f1"
 
 # Limit concurrent builds: https://github.com/google/fuzzbench/pulpl/1212
 
 PYTHONPATH=$FUZZBENCH_HOME python3 $FUZZBENCH_HOME/experiment/run_experiment.py \
     --allow-uncommitted-changes \
-    --experiment-config $SCRIPT_DIR/experiment-config.bean.yaml  \
+    --experiment-config $SCRIPT_DIR/experiment-config.miner.yaml \
     --experiment-name "$name"  \
     --benchmarks "$1" \
     --fuzzers $fuzzers \
